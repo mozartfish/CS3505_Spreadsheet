b0cffdd5c1fdeff73f59cd2b3f86b0e064d5a778
THOMAS ADY <u1120386@lab1-10.eng.utah.edu>
2019-03-27 15:03:24 -0600

    Added declarations for connection code

---
+++ Connection/socket_connections.cpp
@@ -0,0 +1,5 @@
+/*
+ *
+ */
+
+#include "socket_connections.h"
---
+++ Connection/socket_connections.h
@@ -0,0 +1,39 @@
+/*
+ * Authors: Thomas Ady, Pranav Rajan
+ * Last Revision: 3/27/19
+ *
+ * This header encompasses all declarations for socket based connection code
+ * and static functions for connecting, sending, and receiving on TCP based
+ * connections
+ */
+
+#include <sys/socket.h>
+
+#ifndef CONNECT_H
+#define CONNECT_H
+
+public static class socket_connections {
+
+
+
+ public:
+  const static int PORT_NUM = 2112;
+  
+  // Server functions to allow client connection
+  static void WaitForClientConnections();
+  static void ClientConnected();
+
+  // Client functions to allow server connections
+  static void AttemptServerConnection();
+  static void OnServerConnect();
+  
+  // Functions for both Server and CLient to get and process data
+  static void WaitForData();
+  static void OnDataReceived();
+  static void SendData();
+
+  
+
+};
+
+#endif
