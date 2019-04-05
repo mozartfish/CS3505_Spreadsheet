2b74a8e359734f4d1db45a31ec2c5c142646ea2f
Pranav Rajan <rajan.pranav320@gmail.com>
2019-04-05 13:34:27 -0600

    had some compilation problems with the header file which I fixed

--- Server/Dependency_Graph.h
+++ Server/Dependency_Graph.h
@@ -6,6 +6,11 @@
  *These function and class declarations are based upon the CS 3500 dependency graph assignment designed by Professor Daniel Kopta and Professor Joe Zachary
  */
 
+#include <unordered_set>
+#include <unordered_map>
+
+#ifndef DEPENDENCY_GRAPH_H
+#define DEPENDENCY_GRAPH_H
 class DependencyGraph
 {
  private:
@@ -14,11 +19,11 @@ class DependencyGraph
   int numPairs;
   //Key: String that the value set depends on
   //Value: A set of strings that depend on the Key
-  std::unordered_map<std::string, unordered_set<std::string>> dependentsMap;
+  std::unordered_map<std::string, std::unordered_set<std::string>> dependentsMap;
 
   //Key:The string that depends on the value set
   //Value: A set of strings that the Key depends on 
-  std::unordered_map<std::string, unordered_set<std::string>> dependeesMap;
+  std::unordered_map<std::string, std::unordered_set<std::string>> dependeesMap;
 
  public:
   DependencyGraph(); // constructor
@@ -27,11 +32,12 @@ class DependencyGraph
   int Dependees_Size(std::string s); // returns the size of the dependees
   bool Has_Dependents(std::string s); // reports whether dependents is non-empty
   bool Has_Dependees(std::string s); // reports whether dependees is non-empty
-  unordered_set<std::string> Get_Dependents(std::string); // enumerates dependents
-  unordered_set<std::string>Get_Dependees(std::string); // enumerates dependees
+  std::unordered_set<std::string> Get_Dependents(std::string); // enumerates dependents
+  std::unordered_set<std::string>Get_Dependees(std::string); // enumerates dependees
   void Add_Dependency(std::string s, std::string t); // function that adds the ordered pair (s, t) if it doesn't exist
   void Remove_Dependency(std::string s, std::string t); // removes the ordered pair (s, t) if it exists
-  void Replace_Dependents(std::string s, unordered_set<std::string> new_Dependents); // function that removes all ordered pairs of the form (s, r). Then, for each t in new_Dependents, adds the ordered pair (s, t)
-  void Replace_Dependees(std::string s, unordered_set<std::string> new_Dependees); // function that removes all existsing ordered pairs of the form (r, s). Then, for each t in new_Dependees, adds the ordered pair (t, s)
+  void Replace_Dependents(std::string s, std::unordered_set<std::string> new_Dependents); // function that removes all ordered pairs of the form (s, r). Then, for each t in new_Dependents, adds the ordered pair (s, t)
+  void Replace_Dependees(std::string s, std::unordered_set<std::string> new_Dependees); // function that removes all existsing ordered pairs of the form (r, s). Then, for each t in new_Dependees, adds the ordered pair (t, s)
 };
+#endif
 
