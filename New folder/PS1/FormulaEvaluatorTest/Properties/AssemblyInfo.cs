cd309a2b224cd34d704d2a1b3009779192e95412
ThisIsMyUse <u0948300@umail.utah.edu>
2019-04-20 10:25:14 -0600

    added all the previous code from Carina Imburgia, (She rocks, and thanks for the code)

---
+++ New folder/PS1/FormulaEvaluatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FormulaEvaluatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FormulaEvaluatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aeb416d2-6f47-4e2e-8589-172fb5065aea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
