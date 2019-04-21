218e012fd565c9580cd6efe57b9ad452058ffa19
ThisIsMyUse <u0948300@umail.utah.edu>
2019-04-21 16:05:56 -0600

    Removed all the test buttons. Have yet to remove old non used code. Still need to make SSupdates apear at the top of the list.

--- Admin User Interface/WindowsFormsApp1/Form1.Designer.cs
+++ Admin User Interface/WindowsFormsApp1/Form1.Designer.cs
@@ -36,9 +36,6 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.currentStatusList = new System.Windows.Forms.ListBox();
             this.updateList = new System.Windows.Forms.ListBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
             this.IP = new System.Windows.Forms.TextBox();
             this.Port = new System.Windows.Forms.TextBox();
@@ -49,7 +46,7 @@
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(1614, 402);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(350, 160);
             this.button1.TabIndex = 0;
@@ -61,7 +58,7 @@
             // 
             this.button3.BackColor = System.Drawing.SystemColors.MenuText;
             this.button3.Location = new System.Drawing.Point(1614, 950);
-            this.button3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(350, 150);
             this.button3.TabIndex = 2;
@@ -71,7 +68,7 @@
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(1614, 658);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(350, 160);
             this.button2.TabIndex = 3;
@@ -83,7 +80,7 @@
             // 
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.Location = new System.Drawing.Point(1662, 969);
-            this.button5.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button5.Margin = new System.Windows.Forms.Padding(6);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(248, 108);
             this.button5.TabIndex = 10;
@@ -94,7 +91,7 @@
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(24, 40);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(846, 31);
@@ -105,7 +102,7 @@
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(886, 40);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
             this.textBox2.Size = new System.Drawing.Size(646, 31);
@@ -118,7 +115,7 @@
             this.currentStatusList.FormattingEnabled = true;
             this.currentStatusList.ItemHeight = 25;
             this.currentStatusList.Location = new System.Drawing.Point(24, 104);
-            this.currentStatusList.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.currentStatusList.Margin = new System.Windows.Forms.Padding(6);
             this.currentStatusList.Name = "currentStatusList";
             this.currentStatusList.Size = new System.Drawing.Size(846, 1079);
             this.currentStatusList.TabIndex = 13;
@@ -129,48 +126,15 @@
             this.updateList.FormattingEnabled = true;
             this.updateList.ItemHeight = 25;
             this.updateList.Location = new System.Drawing.Point(886, 102);
-            this.updateList.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.updateList.Margin = new System.Windows.Forms.Padding(6);
             this.updateList.Name = "updateList";
             this.updateList.Size = new System.Drawing.Size(646, 1079);
             this.updateList.TabIndex = 14;
             // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(1374, 840);
-            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(132, 73);
-            this.button4.TabIndex = 15;
-            this.button4.Text = "redraw";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(1374, 729);
-            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(132, 69);
-            this.button6.TabIndex = 16;
-            this.button6.Text = "populate model";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(1374, 950);
-            this.button7.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(132, 121);
-            this.button7.TabIndex = 17;
-            this.button7.Text = "test Inserting at top";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.TESTinsertingTopOfList);
-            // 
             // button8
             // 
             this.button8.Location = new System.Drawing.Point(1608, 196);
-            this.button8.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button8.Margin = new System.Windows.Forms.Padding(6);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(350, 87);
             this.button8.TabIndex = 18;
@@ -181,7 +145,7 @@
             // IP
             // 
             this.IP.Location = new System.Drawing.Point(1608, 71);
-            this.IP.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.IP.Margin = new System.Windows.Forms.Padding(6);
             this.IP.Name = "IP";
             this.IP.Size = new System.Drawing.Size(346, 31);
             this.IP.TabIndex = 19;
@@ -189,7 +153,7 @@
             // Port
             // 
             this.Port.Location = new System.Drawing.Point(1608, 146);
-            this.Port.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Port.Margin = new System.Windows.Forms.Padding(6);
             this.Port.Name = "Port";
             this.Port.Size = new System.Drawing.Size(346, 31);
             this.Port.TabIndex = 20;
@@ -224,9 +188,6 @@
             this.Controls.Add(this.Port);
             this.Controls.Add(this.IP);
             this.Controls.Add(this.button8);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button4);
             this.Controls.Add(this.updateList);
             this.Controls.Add(this.currentStatusList);
             this.Controls.Add(this.textBox2);
@@ -235,7 +196,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Administrator Front End";
             this.ResumeLayout(false);
@@ -253,9 +214,6 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.ListBox currentStatusList;
         private System.Windows.Forms.ListBox updateList;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.TextBox IP;
         private System.Windows.Forms.TextBox Port;
